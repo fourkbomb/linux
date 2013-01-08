2dea971d32f3eba37c160e1b9cbb0a1f2aeed290
Samsung OSRC <osrc@samsung.com>
2013-01-08 23:54:22 +0100

    mmc: Soft-patch MoviNAND VTU00M (16GB) eMMC failure
    Signed-off-by: Andrei F <luxneb@gmail.com>

--- include/linux/mmc/core.h
+++ include/linux/mmc/core.h
@@ -154,6 +154,8 @@ extern void mmc_start_bkops(struct mmc_card *card, bool from_exception);
 extern int mmc_switch(struct mmc_card *, u8, u8, u8, unsigned int);
 extern int mmc_send_tuning(struct mmc_host *host, u32 opcode, int *cmd_error);
 extern int mmc_get_ext_csd(struct mmc_card *card, u8 **new_ext_csd);
+extern int mmc_start_movi_smart(struct mmc_card *card);
+extern int mmc_start_movi_operation(struct mmc_card *card);
 
 #define MMC_ERASE_ARG		0x00000000
 #define MMC_SECURE_ERASE_ARG	0x80000000
